f62682fe80aa8b34b7d49c77875a7fbb29c5c527
Sbh <sbh10@hotmail.fr>
2025-11-28 21:07:49 +0100

    Changed URL in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // src/app.js
-const API = "http://localhost:3000/tasks";
+const API = "https://to-do-list-with-backend-h9sm.onrender.com";
 
 const taskList = document.getElementById("taskList");
 const taskinput = document.getElementById("taskinput");
